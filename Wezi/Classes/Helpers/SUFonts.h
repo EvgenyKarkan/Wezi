75264eeac2c295ae02e9ce89c5a03ec360af09e7
Evgeniy Karkan <Evgeniy.Karkan@sigmaukraine.com>
2013-05-30 19:43:34 +0300

    Fonts and fonts Util class added

---
+++ Wezi/Classes/Helpers/SUFonts.h
@@ -0,0 +1,15 @@
+//
+//  SUFonts.h
+//  VeloLink
+//
+//  Copyright (c) 2013 Sigma Ukraine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SUFonts : NSObject
+
++ (UIFont *)plutoSansMediumWithSize:(CGFloat)fontSize;
++ (UIFont *)plutoSansLightWithSize:(CGFloat)fontSize;
+
+@end
---
+++ Wezi/Classes/Helpers/SUFonts.m
@@ -0,0 +1,22 @@
+//
+//  SUFonts.m
+//  VeloLink
+//
+//  Copyright (c) 2013 Sigma Ukraine. All rights reserved.
+//
+
+#import "SUFonts.h"
+
+@implementation SUFonts
+
++ (UIFont *)plutoSansMediumWithSize:(CGFloat)fontSize
+{
+    return [UIFont fontWithName:@"PlutoSansMedium-Italic" size:fontSize];
+}
+
++ (UIFont *)plutoSansLightWithSize:(CGFloat)fontSize
+{
+    return [UIFont fontWithName:@"PlutoSansLight" size:fontSize];
+}
+
+@end
