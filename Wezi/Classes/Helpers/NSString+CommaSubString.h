0fc6dbb47385fb61a8aa861cbb81c94dba40f5d0
EvgenyKarkan <evgenykarkan@gmail.com>
2013-05-31 21:53:38 +0300

    Added category to extract all characters in string after the first comma.

---
+++ Wezi/Classes/Helpers/NSString+CommaSubString.h
@@ -0,0 +1,13 @@
+//
+//  NSString+CommaSubString.h
+//  Wezi
+//
+//  Created by Каркан Евгений on 31.05.13.
+//  Copyright (c) 2013 EvgenyKarkan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (CommaSubString)
+
+@end
---
+++ Wezi/Classes/Helpers/NSString+CommaSubString.m
@@ -0,0 +1,13 @@
+//
+//  NSString+CommaSubString.m
+//  Wezi
+//
+//  Created by Каркан Евгений on 31.05.13.
+//  Copyright (c) 2013 EvgenyKarkan. All rights reserved.
+//
+
+#import "NSString+CommaSubString.h"
+
+@implementation NSString (CommaSubString)
+
+@end
