df4dad5dc78d76f9a028f6ced7dab72e354d0aa3
EvgenyKarkan <evgenykarkan@gmail.com>
2013-07-25 22:20:28 +0300

    All CoreData stuff has been refactored via extract into the own KEDataManager class.

--- Wezi/Classes/Helpers/KEDataManager.m
+++ Wezi/Classes/Helpers/KEDataManager.m
@@ -11,55 +11,118 @@
 
 @implementation KEDataManager
 
+@synthesize managedObjectContext = _managedObjectContext;
+@synthesize managedObjectModel = _managedObjectModel;
+@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
+
 #pragma mark - Singleton stuff
 
 static id _sharedInstance = nil;
 
 + (instancetype)sharedDataManager
 {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        _sharedInstance = [[KEDataManager alloc] init];
-    });
-    
-    return _sharedInstance;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+	    _sharedInstance = [[KEDataManager alloc] init];
+	});
+	
+	return _sharedInstance;
 }
 
 + (id)allocWithZone:(NSZone *)zone
 {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        _sharedInstance = nil;
-        _sharedInstance = [super allocWithZone:zone];
-    });
-    
-    return _sharedInstance;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+	    _sharedInstance = nil;
+	    _sharedInstance = [super allocWithZone:zone];
+	});
+	
+	return _sharedInstance;
 }
 
 - (id)copyWithZone:(NSZone *)zone
 {
-    return self;
+	return self;
 }
 
 #pragma mark - Core Data stuff
 
 - (NSManagedObjectContext *)managedObjectContextFromAppDelegate
 {
-    KEAppDelegate *appDelegate = (KEAppDelegate *)[[UIApplication sharedApplication] delegate];
-    
-    id managedObjectContext = [appDelegate managedObjectContext];
-    
-    return managedObjectContext;
+	return self.managedObjectContext;
 }
 
 - (NSFetchRequest *)requestWithEntityName:(NSString *)entityName
 {
-    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc]init];
-    NSEntityDescription *entity = [NSEntityDescription entityForName:entityName
-                                              inManagedObjectContext:[self managedObjectContextFromAppDelegate]];
-    [fetchRequest setEntity:entity];
-    
-    return fetchRequest;
+	NSFetchRequest *fetchRequest = [[NSFetchRequest alloc]init];
+	NSEntityDescription *entity = [NSEntityDescription entityForName:entityName
+	                                          inManagedObjectContext:[self managedObjectContextFromAppDelegate]];
+	[fetchRequest setEntity:entity];
+	
+	return fetchRequest;
+}
+
+- (void)saveContext
+{
+	NSError *error = nil;
+	NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
+	if (managedObjectContext != nil) {
+		if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
+			NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+			abort();
+		}
+	}
+}
+
+#pragma mark - Core Data stack
+
+- (NSManagedObjectContext *)managedObjectContext
+{
+	if (_managedObjectContext != nil) {
+		return _managedObjectContext;
+	}
+	
+	NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+	if (coordinator != nil) {
+		_managedObjectContext = [[NSManagedObjectContext alloc] init];
+		[_managedObjectContext setPersistentStoreCoordinator:coordinator];
+	}
+	return _managedObjectContext;
+}
+
+- (NSManagedObjectModel *)managedObjectModel
+{
+	if (_managedObjectModel != nil) {
+		return _managedObjectModel;
+	}
+	NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Wezi" withExtension:@"momd"];
+	_managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+	return _managedObjectModel;
+}
+
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
+{
+	if (_persistentStoreCoordinator != nil) {
+		return _persistentStoreCoordinator;
+	}
+	
+	NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"Wezi.sqlite"];
+	
+	NSError *error = nil;
+	_persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+	if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
+		NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+		abort();
+	}
+	
+	return _persistentStoreCoordinator;
+}
+
+#pragma mark - Application's Documents directory
+
+- (NSURL *)applicationDocumentsDirectory
+{
+	return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
 }
 
 @end
--- Wezi/Classes/KEAppDelegate.h
+++ Wezi/Classes/KEAppDelegate.h
@@ -13,12 +13,6 @@
 @interface KEAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (nonatomic, strong)           UIWindow *window;
-@property (nonatomic, readonly, strong) NSManagedObjectContext *managedObjectContext;
-@property (nonatomic, readonly, strong) NSManagedObjectModel *managedObjectModel;
-@property (nonatomic, readonly, strong) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 @property (nonatomic, strong)           AFHTTPClient *client;
-
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
 
 @end
