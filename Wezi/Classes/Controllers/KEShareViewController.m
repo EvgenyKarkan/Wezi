4ef9996d933fbf1a10c3b1e6acc371da0ebd5ed3
EvgenyKarkan <evgenykarkan@gmail.com>
2013-08-13 00:02:23 +0300

    Share view controller has been reworked. Design added.

--- Wezi/Classes/Controllers/KEShareViewController.m
+++ Wezi/Classes/Controllers/KEShareViewController.m
@@ -12,6 +12,12 @@
 
 @property (nonatomic, strong) UIStoryboardPopoverSegue *currentPopoverSegue;
 
+@property (nonatomic, strong) UIButton *twitter;
+@property (nonatomic, strong) UIButton *facebook;
+@property (nonatomic, strong) UIButton *email;
+@property (nonatomic, strong) UIButton *bug;
+@property (nonatomic, strong) NSArray *buttons;
+
 @end
 
 
@@ -19,22 +25,51 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+	[super viewDidLoad];
+	[self createSubViews];
 }
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
+	[super didReceiveMemoryWarning];
+}
+
+#pragma mark - Add buttons
+
+- (void)createSubViews
+{
+	self.twitter = [UIButton buttonWithType:UIButtonTypeCustom];
+	[self.twitter setImage:[UIImage imageNamed:@"twitter"] forState:UIControlStateNormal];
+	[self.twitter setImage:[UIImage imageNamed:@"twitter_pressed"] forState:UIControlStateHighlighted];
+	
+	self.facebook = [UIButton buttonWithType:UIButtonTypeCustom];
+	[self.facebook setImage:[UIImage imageNamed:@"facebook"] forState:UIControlStateNormal];
+	[self.facebook setImage:[UIImage imageNamed:@"facebook_pressed"] forState:UIControlStateHighlighted];
+	
+	self.email = [UIButton buttonWithType:UIButtonTypeCustom];
+	[self.email setImage:[UIImage imageNamed:@"email"] forState:UIControlStateNormal];
+	[self.email setImage:[UIImage imageNamed:@"email_pressed"] forState:UIControlStateHighlighted];
+	
+	self.bug = [UIButton buttonWithType:UIButtonTypeCustom];
+	[self.bug setImage:[UIImage imageNamed:@"Bug-report"] forState:UIControlStateNormal];
+	[self.bug setImage:[UIImage imageNamed:@"Bug_report_pressed"] forState:UIControlStateHighlighted];
+	
+	self.buttons = @[self.twitter, self.facebook, self.email, self.bug];
+	
+	for (NSUInteger i = 0; i < [self.buttons count]; i++) {
+		[self.buttons[i] setFrame:CGRectMake(20.0f, 20.0f + (i * 70.0f), 160.0f, 50.0f)];
+		[self.buttons[i] setTag:100 + (i * 1)];
+		[self.buttons[i] addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+		[self.view addSubview:self.buttons[i]];
+	}
 }
 
 #pragma mark - Actions
 
-- (IBAction)buttonPressed:(id)sender
+- (void)buttonPressed:(id)sender
 {
 	[self.firstDelegate hideSharePopover];
 	[self.secondDelegate provideSocialMediaWithSender:sender];
 }
-
-
 
 @end
