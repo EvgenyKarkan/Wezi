668c55e8baa882da9d1f4407953a8e765c73dc02
EvgenyKarkan <evgenykarkan@gmail.com>
2013-08-31 01:56:16 +0300

    HIG naming conventions added.

--- Wezi/Classes/Helpers/Utils/KEBackingImageUtil.m
+++ Wezi/Classes/Helpers/Utils/KEBackingImageUtil.m
@@ -10,12 +10,12 @@
 
 @implementation KEBackingImageUtil
 
-static NSString *const kKEBlue   = @"color_backg_blue.png";
-static NSString *const kKEBlue2  = @"color_backg_blue2.png";
-static NSString *const kKEGreen  = @"color_backg_green.png";
-static NSString *const kKERed    = @"color_backg_red.png";
-static NSString *const kKEViolet = @"color_backg_violet.png";
-static NSString *const kKEYellow = @"color_backg_yellow.png";
+static NSString * const kKEBlue   = @"color_backg_blue.png";
+static NSString * const kKEBlue2  = @"color_backg_blue2.png";
+static NSString * const kKEGreen  = @"color_backg_green.png";
+static NSString * const kKERed    = @"color_backg_red.png";
+static NSString * const kKEViolet = @"color_backg_violet.png";
+static NSString * const kKEYellow = @"color_backg_yellow.png";
 
 + (NSArray *)arrayWithRainbowImages
 {
--- Wezi/Classes/KEAppDelegate.h
+++ Wezi/Classes/KEAppDelegate.h
@@ -8,7 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-
 @class AFHTTPClient;
 
 @interface KEAppDelegate : UIResponder <UIApplicationDelegate>
